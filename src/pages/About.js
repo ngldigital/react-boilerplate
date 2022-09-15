8f5fc20f25f38c57dc4a254d1e02e7a2448a3fe2
Nigel Lim <nigelflim@gmail.com>
2022-09-15 22:32:19 +1000

    added paragraph

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,6 +4,11 @@ const About = () => {
   return (
     <div>
       <h1>About the new features</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo voluptatem
+        soluta mollitia amet, deserunt cum a dolore, aliquam nulla, quisquam
+        corporis perferendis ipsam quis non esse sit voluptates sed tenetur?
+      </p>
     </div>
   );
 };
