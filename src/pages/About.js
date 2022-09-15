784398e9de084c1003694d920e715d70d6977f5e
Nigel Lim <nigelflim@gmail.com>
2022-09-15 23:08:04 +1000

    added more text

--- src/pages/About.js
+++ src/pages/About.js
@@ -9,6 +9,7 @@ const About = () => {
         soluta mollitia amet, deserunt cum a dolore, aliquam nulla, quisquam
         corporis perferendis ipsam quis non esse sit voluptates sed tenetur?
       </p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad error itaque magnam architecto, deleniti aut distinctio sint pariatur, inventore omnis hic, non esse est? Magnam itaque sapiente veniam corporis minus!</p>
     </div>
   );
 };
