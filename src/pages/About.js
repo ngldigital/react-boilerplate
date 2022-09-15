9181fa120a8bfe2b2d68538f23f7d76b3eb8542b
Nigel Lim <nigelflim@gmail.com>
2022-09-15 22:59:25 +1000

    Added about

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore
+        atque laboriosam aperiam maiores sequi tempora commodi natus ipsum
+        possimus minima. Ipsam distinctio harum ex iure facilis impedit eos
+        facere doloremque!
+      </p>
+    </div>
+  );
 };
 
 export default About;
