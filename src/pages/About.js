b435263831346211b86a5f7c0bc6672b7b451f3e
Nigel Lim <nigelflim@gmail.com>
2022-09-15 23:05:36 +1000

    added h1

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,18 +3,12 @@ import React from "react";
 const About = () => {
   return (
     <div>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore
-        atque laboriosam aperiam maiores sequi tempora commodi natus ipsum
-        possimus minima. Ipsam distinctio harum ex iure facilis impedit eos
-        facere doloremque!
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed
-        voluptatibus blanditiis quas iure. Repellendus at inventore saepe fugiat
-        natus iste debitis doloremque delectus? Numquam voluptatibus eaque ipsa
-        repudiandae excepturi.
-      </p>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic adipisci
+        voluptates totam deserunt natus illum quae provident, saepe harum
+        molestias cumque explicabo laudantium aliquam cum ea eum voluptas. Odio,
+        repellat?
+      </h1>
     </div>
   );
 };
