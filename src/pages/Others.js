f99158ddb8372027bc3fb8e97c804479b5f8374d
Nigel Lim <nigelflim@gmail.com>
2022-09-15 21:49:32 +1000

    Added others branch

--- src/pages/Others.js
+++ src/pages/Others.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Others = () => {
-  return <div>Others</div>;
+  return (
+    <div>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum ipsa
+        suscipit distinctio laborum beatae aspernatur possimus impedit optio,
+        consequatur eos voluptatum blanditiis maiores dolores qui, ad fugiat
+        saepe ut omnis.
+      </h1>
+    </div>
+  );
 };
 
 export default Others;
