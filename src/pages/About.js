f8085d97d389e4fe5b1c92bbfb8a80d51ec76349
Nigel Lim <nigelflim@gmail.com>
2022-09-15 23:01:14 +1000

    added about

--- src/pages/About.js
+++ src/pages/About.js
@@ -9,6 +9,12 @@ const About = () => {
         possimus minima. Ipsam distinctio harum ex iure facilis impedit eos
         facere doloremque!
       </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed
+        voluptatibus blanditiis quas iure. Repellendus at inventore saepe fugiat
+        natus iste debitis doloremque delectus? Numquam voluptatibus eaque ipsa
+        repudiandae excepturi.
+      </p>
     </div>
   );
 };
